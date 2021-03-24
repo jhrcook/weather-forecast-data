a5d481841f65ab826cfb77ee5dfc7979a0a0b81c
Josh Cook <joshuacook0023@gmail.com>
2021-03-24 08:46:44 -0400

    fix: pass API keys using CLI

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ import json
 from datetime import datetime
 from pathlib import Path
 from pprint import pprint
-from typing import Callable
+from typing import Callable, Optional
 
 import typer
+from click.core import Option
 from pydantic.main import BaseModel
 from weather_forecast_collection.apis import accuweather_api as accu
 from weather_forecast_collection.apis import climacell_api as cc
 from weather_forecast_collection.apis import national_weather_service_api as nws
 from weather_forecast_collection.apis import openweathermap_api as owm
 
-import keys
+try:
+    import keys
+except:
+    print("Module 'keys' not found - API keys must be passed by CLI.")
 from coordinates import get_coordinates
 
 app = typer.Typer()
@@ -52,44 +56,66 @@ def national_weather_service(city: str) -> Path:
 
 
 @app.command()
-def accuweather(city: str) -> Path:
+def accuweather(city: str, api_key: Optional[str] = None) -> Path:
+    if api_key is None:
+        try:
+            api_key = keys.accuweather_api_key
+        except Exception as err:
+            print(err)
+            raise ValueError("Accuweather key error.")
+
     coords = get_coordinates(city)
     forecast = accu.get_accuweather_forecast(
-        lat=coords.lat, long=coords.long, api_key=keys.accuweather_api_key
+        lat=coords.lat, long=coords.long, api_key=api_key
     )
     fp = save_data(source="accuweather", city=city, data=forecast)
     return fp
 
 
 @app.command()
-def open_weather_map(city: str) -> Path:
+def open_weather_map(city: str, api_key: Optional[str] = None) -> Path:
+    if api_key is None:
+        try:
+            api_key = keys.openweathermap_api_key
+        except Exception as err:
+            print(err)
+            raise ValueError("OpenWeatherMap key error.")
+
     coords = get_coordinates(city)
     forecast = owm.get_openweathermap_data(
-        lat=coords.lat, long=coords.long, api_key=keys.openweathermap_api_key
+        lat=coords.lat, long=coords.long, api_key=api_key
     )
     fp = save_data("open-weather-map", city, forecast)
     return fp
 
 
 @app.command()
-def climacell(city: str) -> Path:
+def climacell(city: str, api_key: Optional[str] = None) -> Path:
+    if api_key is None:
+        try:
+            api_key = keys.climacell_api_key
+        except Exception as err:
+            print(err)
+            raise ValueError("ClimaCell key error.")
+
     coords = get_coordinates(city)
-    forecast = cc.get_climacell_data(
-        lat=coords.lat, long=coords.long, api_key=keys.climacell_api_key
-    )
+    forecast = cc.get_climacell_data(lat=coords.lat, long=coords.long, api_key=api_key)
     fp = save_data("climacell", city, forecast)
     return fp
 
 
 @app.command()
-def all_data(city: str):
+def all_data(
+    city: str,
+    accu_key: Optional[str] = None,
+    owm_key: Optional[str] = None,
+    cc_key: Optional[str] = None,
+):
     _ = national_weather_service(city=city)
-    _ = accuweather(city=city)
-    _ = open_weather_map(city=city)
-    _ = climacell(city=city)
+    _ = accuweather(city=city, api_key=accu_key)
+    _ = open_weather_map(city=city, api_key=owm_key)
+    _ = climacell(city=city, api_key=cc_key)
 
 
 if __name__ == "__main__":
     app()
-
-# TODO: GitHub Action to run script and store data.
